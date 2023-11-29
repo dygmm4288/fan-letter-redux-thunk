402b501f497f8586682c1988e66311874f8dfda4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-29 23:23:24 +0900

    [UPDATE] 라우터 재설정

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Detail from "./Detail";
-import Home from "./Home";
+import Login from "components/auth/Login";
+import SignUp from "components/auth/SignUp";
+import { selectAuth } from "modules/auth/authSlice";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
 export default function Router() {
+  const { isLogin } = useSelector(selectAuth);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/detail/:memberName/:id' element={<Detail />} />
-        <Route path='*' element={<Detail />} />
+        <Route element={isLogin ? <Layout /> : <Navigate to='login' />}>
+          <Route path='/' element={<div>Home</div>} />
+          <Route path='/details/:id' element={<div>상세화면</div>} />
+        </Route>
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
