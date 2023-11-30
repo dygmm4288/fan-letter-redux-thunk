f0780be49f24ac900e5673f5a81453a1a63bfb47
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-30 21:56:16 +0900

    [ADD] Sign Up 기능 추가

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -3,6 +3,7 @@ import { registerThunk, selectAuth } from "modules/auth/authSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function SignUp() {
   const navigate = useNavigate();
@@ -26,40 +27,81 @@ export default function SignUp() {
   };
 
   return (
-    <form onSubmit={handleSignUp}>
-      <h1>회원가입</h1>
-      <input
-        name='id'
-        type='text'
-        placeholder='아이디 (4~10글자)'
-        minLength={4}
-        maxLength={10}
-        required
-        value={id}
-        onChange={handleChangeId}
-      />
-      <input
-        name='password'
-        type='password'
-        placeholder='비밀번호 (4~15글자)'
-        minLength={4}
-        maxLength={15}
-        required
-        value={password}
-        onChange={handleChangePassword}
-      />
-      <input
-        name='nickname'
-        type='text'
-        placeholder='닉네임 (1~10글자)'
-        minLength={1}
-        maxLength={10}
-        required
-        value={nickname}
-        onChange={handleChangeNickname}
-      />
-      <button type='submit'>회원가입</button>
-      <Link to='/login'>로그인</Link>
-    </form>
+    <StLoginWrapper>
+      <StLoginForm onSubmit={handleSignUp}>
+        <StLoginHeader>회원가입</StLoginHeader>
+        <StInput
+          name='id'
+          type='text'
+          placeholder='아이디 (4~10글자)'
+          minLength={4}
+          maxLength={10}
+          required
+          value={id}
+          onChange={handleChangeId}
+        />
+        <StInput
+          name='password'
+          type='password'
+          placeholder='비밀번호 (4~15글자)'
+          minLength={4}
+          maxLength={15}
+          required
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <StInput
+          name='nickname'
+          type='text'
+          placeholder='닉네임 (1~10글자)'
+          minLength={1}
+          maxLength={10}
+          required
+          value={nickname}
+          onChange={handleChangeNickname}
+        />
+        <StLoginButton type='submit'>회원가입</StLoginButton>
+        <Link to='/login'>로그인</Link>
+      </StLoginForm>
+    </StLoginWrapper>
   );
 }
+const StLoginWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  overflow: hidden;
+  display: flex;
+  > * {
+    margin: auto;
+  }
+  background-color: lightgray;
+`;
+const StLoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-between;
+
+  gap: 1rem;
+
+  padding: 1rem;
+  width: 450px;
+  height: 300px;
+  border-radius: 0.5rem;
+
+  background-color: white;
+
+  * {
+    text-align: center;
+  }
+`;
+const StLoginHeader = styled.h1`
+  font-size: 2.5rem;
+`;
+
+const StInput = styled.input`
+  outline: none;
+  padding: 0.5rem;
+`;
+const StLoginButton = styled.button``;
